d2df0cb72611f214e76eabefcf9852b73491538e
dukisima <dukisima02@gmail.com>
2025-09-30 21:16:29 +0200

    Created a new function
    This is the start for the picture degradations experiment

---
+++ src/degradations.py
@@ -0,0 +1,11 @@
+#here I will have functions for degrading the whole data set to check how that effect the model performance
+import os
+
+
+class Degrad():
+    def __innit__(self, img):
+        self.img = img
+        pass
+
+    def gauss_noise(self, img):
+        pass
